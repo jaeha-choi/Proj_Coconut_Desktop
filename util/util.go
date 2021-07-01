9c5f07b762191f2652174017e2691fb5046caed0
Jaeha Choi <jaeha220@gmail.com>
2021-07-01 09:40:13 -0700

    Add function/tests to read string

--- util/util.go
+++ util/util.go
@@ -1,1 +1,56 @@
 package util
+
+import (
+	"github.com/jaeha-choi/Proj_Coconut_Utility/log"
+	"io"
+)
+
+const (
+	bufferSize = 4096
+)
+
+// readNString reads up to nth character. Maximum buffer size does not exceed bufferSize.
+func readNString(reader io.Reader, n uint32) (string, error) {
+	var buffSize uint32
+	var totalReceived uint32 = 0
+	resultString := ""
+
+	if n < bufferSize {
+		buffSize = n
+	} else {
+		buffSize = bufferSize
+	}
+
+	buffer := make([]byte, buffSize)
+
+	var recv int
+	var err error
+	for totalReceived < n {
+		if totalReceived+buffSize > n {
+			buffer, err = io.ReadAll(io.LimitReader(reader, int64(n-totalReceived)))
+			recv = len(buffer)
+		} else {
+			recv, err = io.ReadFull(reader, buffer)
+		}
+		if err != nil {
+			log.Warning("Error while receiving string")
+			return "", err
+		}
+		resultString += string(buffer)
+		totalReceived += uint32(recv)
+	}
+	return resultString, nil
+}
+
+// readNString2 is similar to readNString except that there is no limit on buffer size.
+func readNString2(reader io.Reader, n uint32) (string, error) {
+	buffer, err := readNBytes(reader, n)
+	return string(buffer), err
+}
+
+// readNBytes reads up to nth byte
+func readNBytes(reader io.Reader, n uint32) ([]byte, error) {
+	buffer := make([]byte, n)
+	_, err := io.ReadFull(reader, buffer)
+	return buffer, err
+}
--- util/util_test.go
+++ util/util_test.go
@@ -1,1 +1,60 @@
 package util
+
+import (
+	"github.com/jaeha-choi/Proj_Coconut_Utility/log"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestInit(t *testing.T) {
+	log.Init(os.Stdout, log.DEBUG)
+}
+
+func TestReadNString(t *testing.T) {
+	reader := strings.NewReader("init 1234 prev 5678 curr next \n")
+	s, _ := readNString(reader, 22)
+	if s != "init 1234 prev 5678 cu" {
+		t.Error("Incorrect result.")
+	}
+}
+
+func TestReadNStringZero(t *testing.T) {
+	reader := strings.NewReader("init 1234 prev 5678 curr next \n")
+	s, _ := readNString(reader, 0)
+	if s != "" {
+		t.Error("Incorrect result.")
+	}
+}
+
+func TestReadNStringExceed(t *testing.T) {
+	reader := strings.NewReader("init 1234 prev 5678 curr next \n")
+	_, err := readNString(reader, 50)
+	if err == nil {
+		t.Error("Excepted error, but no error raised.")
+	}
+}
+
+func TestReadNString2(t *testing.T) {
+	reader := strings.NewReader("init 1234 prev 5678 curr next \n")
+	s, _ := readNString2(reader, 22)
+	if s != "init 1234 prev 5678 cu" {
+		t.Error("Incorrect result.")
+	}
+}
+
+func TestReadNString2Exceed(t *testing.T) {
+	reader := strings.NewReader("init 1234 prev 5678 curr next \n")
+	_, err := readNString2(reader, 50)
+	if err == nil {
+		t.Error("Excepted error, but no error raised.")
+	}
+}
+
+func TestReadNString2Zero(t *testing.T) {
+	reader := strings.NewReader("init 1234 prev 5678 curr next \n")
+	s, _ := readNString(reader, 0)
+	if s != "" {
+		t.Error("Incorrect result.")
+	}
+}
