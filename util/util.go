97992667c1ab44fa5091f60951cb8afd60714cf1
robin <robinseo.seo@gmail.com>
2021-07-02 17:23:18 +0900

    fix CheckIPAddress function

--- util/util.go
+++ util/util.go
@@ -6,11 +6,5 @@ import (
 
 // TestCheckIPAddress check if ip address is valid or not
 func CheckIPAddress(ip string) bool {
-	if net.ParseIP(ip) == nil {
-		//log.Error("IP Address: " + ip + "- Invalid\n")
-		return false
-	} else {
-		//log.Error("IP Address: " + ip + " - Valid\n")
-		return true
-	}
+	return net.ParseIP(ip) != nil
 }
--- util/util_test.go
+++ util/util_test.go
@@ -2,26 +2,30 @@ package util
 
 import "testing"
 
-func TestCheckIPAddress(t *testing.T) {
+func TestCheckIPAddressValidIpv4(t *testing.T) {
 	validIPV4 := "10.40.210.253"
-	CheckIPAddress(validIPV4)
-	if CheckIPAddress(validIPV4) == false {
-		t.Error("IP Address is invalid")
+	if !CheckIPAddress(validIPV4) {
+		t.Error("TestCheckIPAddressValidIpv4 is invalid")
 	}
+}
 
+func TestCheckIPAddressInvalidIpv4(t *testing.T) {
 	invalidIPV4 := "1000.40.210.253"
-	CheckIPAddress(invalidIPV4)
-	if CheckIPAddress(invalidIPV4) == false {
-		t.Error("IP Address is invalid")
+	if CheckIPAddress(invalidIPV4) {
+		t.Error("TestCheckIPAddressInvalidIpv4 is invalid")
 	}
-	validIPV6 := "2001:0db8:85a3:0000:0000:8a2e:0370:7334"
-	CheckIPAddress(validIPV6)
-	if CheckIPAddress(validIPV6) == false {
-		t.Error("IP Address is invalid")
-	}
+}
 
+func TestCheckIPAddressValidIpv6(t *testing.T) {
+	validIPV6 := "2001:0db8:85a3:0000:0000:8a2e:0370:7334"
+	if !CheckIPAddress(validIPV6) {
+		t.Error("TestCheckIPAddressValidIpv6 is invalid")
+	}
+}
+
+func TestCheckIPAddressInvalidIpv6(t *testing.T) {
 	invalidIPV6 := "2001:0db8:85a3:0000:0000:8a2e:0370:7334:3445"
-	if CheckIPAddress(invalidIPV6) == false {
-		t.Error("IP Address is invalid")
+	if CheckIPAddress(invalidIPV6) {
+		t.Error("TestCheckIPAddressInvalidIpv6 is invalid")
 	}
 }
