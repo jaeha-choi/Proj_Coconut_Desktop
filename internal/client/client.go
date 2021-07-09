5f5fc5d2e62acdd097d27fc03941a81b124616c5
Jaeha Choi <jaeha220@gmail.com>
2021-07-09 16:31:39 -0700

    Add OpenKeys
    OpenKeys open keys if they exist, or create and save new keys if they don't exist.

--- internal/client/client.go
+++ internal/client/client.go
@@ -5,14 +5,12 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
-	"errors"
 	"github.com/jaeha-choi/Proj_Coconut_Utility/log"
+	"io/ioutil"
 	"os"
 )
 
-var ExistingKeyError = errors.New("keys already exist")
-
-func CreateRSAKey(bitSize int) (*rsa.PrivateKey, error) {
+func createRSAKey(bitSize int) (*rsa.PrivateKey, error) {
 	reader := rand.Reader
 	key, err := rsa.GenerateKey(reader, bitSize)
 	if err != nil {
@@ -23,86 +21,125 @@ func CreateRSAKey(bitSize int) (*rsa.PrivateKey, error) {
 	return key, nil
 }
 
-func SaveRSAKeys(key *rsa.PrivateKey) error {
+func OpenKeys() (*pem.Block, *pem.Block, error) {
+	var err error
+	var pubBlock *pem.Block
+	var privBlock *pem.Block
+	var newKeyBitSize = 4096
 	pubFileN := "key.pub"
 	privFileN := "key.priv"
-	var err error = nil
 
 	// Check if keys already exist
-	if _, err := os.Stat(pubFileN); err == nil {
-		log.Debug(err)
-		return ExistingKeyError
-	}
-	// Open pubkey file
-	pubOut, err := os.Create(pubFileN)
-	if err != nil {
-		log.Debug(err)
-		log.Error("Error while opening file")
-		return err
-	}
-	// Close pubkey file when done
-	defer func() {
-		if errDef := pubOut.Close(); errDef != nil {
-			log.Error(err)
-			log.Error("Error while closing pubkey file")
-			err = errDef
-			return
-		}
-		if errDef := os.Chmod(pubFileN, 0644); errDef != nil {
-			log.Debug(errDef)
-			log.Error("Error while updating permissions")
-			err = errDef
-			return
-		}
-	}()
-	// Create PEM block
-	block := pem.Block{
-		Type:    "RSA PUBLIC KEY",
-		Headers: nil,
-		Bytes:   x509.MarshalPKCS1PublicKey(&key.PublicKey),
-	}
-	// Write to pubkey file
-	err = pem.Encode(pubOut, &block)
-	if err != nil {
-		log.Debug(err)
-		log.Error("Error while encoding public key")
-		return err
-	}
+	if _, err := os.Stat(pubFileN); !os.IsNotExist(err) {
 
-	// Create priv key file
-	privOut, err := os.OpenFile(privFileN, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0666)
-	if err != nil {
-		log.Debug(err)
-		log.Error("Error while creating private key")
-		return err
-	}
-	defer func() {
-		if errDef := privOut.Close(); errDef != nil {
-			log.Debug(errDef)
-			log.Error("Error while closing private key file")
-			err = errDef
-			return
+		// Read existing pubkey file
+		pubFileBytes, err := ioutil.ReadFile(pubFileN)
+		if err != nil {
+			log.Debug(err)
+			log.Error("Error while opening RSA pubkey in OpenKeys")
+			return nil, nil, err
 		}
-		if errDef := os.Chmod(privFileN, 0600); errDef != nil {
-			log.Debug(errDef)
-			log.Error("Error while updating permissions")
-			err = errDef
-			return
+		// Decode file and create block
+		pubBlock, _ = pem.Decode(pubFileBytes)
+		if pubBlock == nil {
+			log.Error("Error while decoding pubkey file")
+			return nil, nil, err
 		}
-	}()
-	// Create PEM block
-	block = pem.Block{
-		Type:    "RSA PRIVATE KEY",
-		Headers: nil,
-		Bytes:   x509.MarshalPKCS1PrivateKey(key),
-	}
-	// Write to priv file
-	err = pem.Encode(privOut, &block)
-	if err != nil {
-		log.Debug(err)
-		log.Error("Error while encoding private key")
-		return err
-	}
 
-	return err
+		// Read existing privkey file
+		privFileBytes, err := ioutil.ReadFile(privFileN)
+		if err != nil {
+			log.Debug(err)
+			log.Error("Error while opening RSA privfile in OpenKeys")
+			return nil, nil, err
+		}
+		// Decode file and create block
+		privBlock, _ = pem.Decode(privFileBytes)
+		if privBlock == nil {
+			log.Error("Error while decoding priv file")
+			return nil, nil, err
+		}
+	} else if os.IsNotExist(err) {
+		log.Debug(err)
+		// Create new RSA key pair
+		key, err := createRSAKey(newKeyBitSize)
+		if err != nil {
+			log.Debug(err)
+			log.Error("Error while creating RSA key in OpenKeys")
+			return nil, nil, err
+		}
+		// Open pubkey file
+		pubOut, err := os.Create(pubFileN)
+		if err != nil {
+			log.Debug(err)
+			log.Error("Error while creating pubkey file")
+			return nil, nil, err
+		}
+		// Close pubkey file when done
+		defer func() {
+			if errDef := pubOut.Close(); errDef != nil {
+				log.Error(err)
+				log.Error("Error while closing pubkey file")
+				err = errDef
+				return
+			}
+			if errDef := os.Chmod(pubFileN, 0644); errDef != nil {
+				log.Debug(errDef)
+				log.Error("Error while updating pubkey file permissions")
+				err = errDef
+				return
+			}
+		}()
+		// Create PEM block
+		pubBlock = &pem.Block{
+			Type:    "RSA PUBLIC KEY",
+			Headers: nil,
+			Bytes:   x509.MarshalPKCS1PublicKey(&key.PublicKey),
+		}
+		// Write PEM block to pubkey file
+		if err := pem.Encode(pubOut, pubBlock); err != nil {
+			log.Debug(err)
+			log.Error("Error while writing to pubkey file")
+			return nil, nil, err
+		}
+
+		// Create privkey file
+		privOut, err := os.OpenFile(privFileN, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0666)
+		if err != nil {
+			log.Debug(err)
+			log.Error("Error while creating private key in OpenKeys")
+			return nil, nil, err
+		}
+		// Close privkey file when done
+		defer func() {
+			if errDef := privOut.Close(); errDef != nil {
+				log.Debug(errDef)
+				log.Error("Error while closing private key file")
+				err = errDef
+				return
+			}
+			if errDef := os.Chmod(privFileN, 0600); errDef != nil {
+				log.Debug(errDef)
+				log.Error("Error while updating permissions")
+				err = errDef
+				return
+			}
+		}()
+		// Create PEM block
+		privBlock = &pem.Block{
+			Type:    "RSA PRIVATE KEY",
+			Headers: nil,
+			Bytes:   x509.MarshalPKCS1PrivateKey(key),
+		}
+		// Write PEM block to privkey file
+		if err := pem.Encode(privOut, privBlock); err != nil {
+			log.Debug(err)
+			log.Error("Error while encoding private key")
+			return nil, nil, err
+		}
+	} else {
+		log.Debug(err)
+		log.Error("Error while checking stat of the file")
+	}
+	return pubBlock, privBlock, err
 }
--- internal/client/client_test.go
+++ internal/client/client_test.go
@@ -11,26 +11,30 @@ func init() {
 }
 
 func TestCreateRSAKey(t *testing.T) {
-	t.Cleanup(func() {
-		pubFileN := "key.pub"
-		privFileN := "key.priv"
-		if err := os.Remove(pubFileN); err != nil {
-			t.Error("Could not remove public key")
-		}
-		if err := os.Remove(privFileN); err != nil {
-			t.Error("Could not remove private key")
-		}
-	})
+	//t.Cleanup(func() {
+	//	pubFileN := "key.pub"
+	//	privFileN := "key.priv"
+	//	if err := os.Remove(pubFileN); err != nil {
+	//		t.Error("Could not remove public key")
+	//	}
+	//	if err := os.Remove(privFileN); err != nil {
+	//		t.Error("Could not remove private key")
+	//	}
+	//})
 	bitSize := 1024
-	key, err := CreateRSAKey(bitSize)
+	key, err := createRSAKey(bitSize)
 	if err != nil || key.N.BitLen() != bitSize {
 		log.Debug(err)
-		t.Error("Error in CreateRSAKey")
+		t.Error("Error in createRSAKey")
 		return
 	}
-	if err := SaveRSAKeys(key); err != nil {
+}
+
+func TestOpenKeys(t *testing.T) {
+	pubPem, privPem, err := OpenKeys()
+	if pubPem == nil || privPem == nil || err != nil {
 		log.Debug(err)
-		t.Error("Error in SaveRSAKeys")
+		t.Error("Error in OpenKeys")
 		return
 	}
 }
