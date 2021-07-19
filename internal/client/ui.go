570d9d11493087056eaa025785bdf71c30b338e1
Jaeha Choi <jaeha220@gmail.com>
2021-07-18 22:44:37 -0700

    Add action to status button, info box, and add device

--- internal/client/ui.go
+++ internal/client/ui.go
@@ -9,12 +9,14 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"time"
 )
 
 const (
 	appId = "dev.jaeha.coconut"
 )
 
+// File tree view index
 const (
 	fileNameIdx = iota
 	fileSizeWithUnitIdx
@@ -23,23 +25,36 @@ const (
 	fileSizeInBytes
 )
 
+// Contact tree view index
+const (
+	keyName = iota
+	keyDate
+	keyFingerprint
+)
+
 var AssertFailed = errors.New("type assertion failed")
 
 type UIStatus struct {
-	builder       *gtk.Builder
-	isFileTab     bool
-	fileListOrder []int
-	fileMap       map[string]struct{}
+	builder        *gtk.Builder
+	isFileTab      bool
+	onlineStatus   bool
+	fileListOrder  []int
+	keyListOrder   []int
+	totalFileSize  int64
+	totalFileCount int
+	fileMap        map[string]struct{}
 }
-
-type preprocessing func(string) ([]interface{}, error)
 
 func initUIStatus() (stat *UIStatus) {
 	return &UIStatus{
-		builder:       nil,
-		isFileTab:     true,
-		fileListOrder: []int{fileNameIdx, fileSizeWithUnitIdx, fileStatusIdx, fileFullPath, fileSizeInBytes},
-		fileMap:       map[string]struct{}{},
+		builder:        nil,
+		isFileTab:      true,
+		onlineStatus:   false,
+		fileListOrder:  []int{fileNameIdx, fileSizeWithUnitIdx, fileStatusIdx, fileFullPath, fileSizeInBytes},
+		keyListOrder:   []int{keyName, keyDate, keyFingerprint},
+		totalFileSize:  0,
+		totalFileCount: 0,
+		fileMap:        map[string]struct{}{},
 	}
 }
 
@@ -76,9 +91,12 @@ func Start(uiGladePath string) {
 		// Map the handlers to callback functions, and connect the signals
 		// to the Builder.
 		signals := map[string]interface{}{
-			"switchPage":     stat.switchPage,
-			"addButtonClick": stat.addButtonClick,
-			"keyPress":       stat.keyPress,
+			"switchPage":         stat.switchPage,
+			"addButtonClick":     stat.addButtonClick,
+			"keyPressMainWin":    stat.keyPressMainWin,
+			"statusClick":        stat.statusClick,
+			"addCodeDone":        stat.addCodeDone,
+			"clickEmptySpotFile": stat.clickEmptySpotFile,
 		}
 		stat.builder.ConnectSignals(signals)
 
@@ -128,24 +146,118 @@ func (ui *UIStatus) addButtonClick() {
 				log.Error("Error while getting filenames")
 				return
 			}
-			fileList, err := ui.getListStoreWithId("fileList")
-			if err != nil {
-				return
-			}
-			treeView, err := ui.getTreeViewWithId("fileListView")
-			if err != nil {
-				return
-			}
-
-			addToListStore(fileList, treeView, filenames, ui.fileListOrder, ui.getFileInfo)
+			ui.addFilesToListStore(filenames)
 		}
 
 	} else {
 		// Add contacts
+		ui.showAddCodeEntry()
 	}
 }
 
-func (ui *UIStatus) keyPress(_ *gtk.Window, event *gdk.Event) {
+func (ui *UIStatus) statusClick(_ *gtk.EventBox, event *gdk.Event) {
+	log.Debug("Status label clicked")
+	eventButton := gdk.EventButtonNewFromEvent(event)
+	// If user right-clicks the status label
+	if gdk.BUTTON_PRIMARY == eventButton.Button() {
+		label, err := ui.getLabelWithId("connStatusLabel")
+		if err != nil {
+			return
+		}
+		listBoxRow, err := ui.getListBoxRowWithId("statusBoxRow")
+		if err != nil {
+			return
+		}
+		// Make status label un-clickable
+		listBoxRow.SetSensitive(false)
+		log.Debug(ui.onlineStatus)
+		if ui.onlineStatus {
+			label.SetMarkup("<span foreground=\"orange\">Disconnecting...</span>")
+			go func() {
+				// TODO: Disconnect user from the relay server
+				time.Sleep(3 * time.Second)
+
+				_ = glib.IdleAdd(func() {
+					label.SetMarkup("<span foreground=\"red\">Offline</span>")
+					ui.onlineStatus = !ui.onlineStatus
+					listBoxRow.SetSensitive(true)
+				})
+			}()
+
+		} else {
+			label.SetMarkup("<span foreground=\"orange\">Connecting...</span>")
+			go func() {
+				// TODO: Connect user to the relay server
+				time.Sleep(3 * time.Second)
+
+				_ = glib.IdleAdd(func() {
+					label.SetMarkup("<span foreground=\"green\">Online</span>")
+					ui.onlineStatus = !ui.onlineStatus
+					listBoxRow.SetSensitive(true)
+				})
+			}()
+		}
+
+	}
+}
+
+func (ui *UIStatus) showAddCodeEntry() {
+	log.Debug("showAddCodeEntry called")
+	popover, err := ui.getPopoverWithId("addCodeEntry")
+	if err != nil {
+		return
+	}
+	popover.Popup()
+}
+
+func (ui *UIStatus) addCodeDone(entry *gtk.Entry, event *gdk.Event) {
+	log.Debug("addCodeDone called")
+	eventKey := gdk.EventKeyNewFromEvent(event)
+	key := eventKey.KeyVal()
+	if key == gdk.KEY_Return || key == gdk.KEY_KP_Enter {
+		text, err := entry.GetText()
+		if err != nil {
+			return
+		}
+		intVal, err := strconv.ParseInt(text, 10, 32)
+		if err != nil {
+			// text is not an integer
+			log.Debug("Not an integer: ", text)
+			return
+		}
+		popover, err := ui.getPopoverWithId("addCodeEntry")
+		if err != nil {
+			return
+		}
+		popover.Popdown()
+		entry.SetText("")
+		// TODO: Get pubkey based on intVal
+		log.Debug(intVal)
+	}
+}
+
+func (ui *UIStatus) clickEmptySpotFile(_ *gtk.EventBox, event *gdk.Event) {
+	log.Debug("clickEmptySpotFile called")
+	eventButton := gdk.EventButtonNewFromEvent(event)
+	if eventButton.Button() == gdk.BUTTON_PRIMARY {
+		fileTreeView, err := ui.getTreeViewWithId("fileListView")
+		if err != nil {
+			return
+		}
+		ui.unselectAll(fileTreeView)
+	}
+}
+
+func (ui *UIStatus) unselectAll(treeView *gtk.TreeView) {
+	selection, err := treeView.GetSelection()
+	if err != nil {
+		log.Debug(err)
+		log.Error("Error while getting selected files")
+	}
+	selection.UnselectAll()
+}
+
+func (ui *UIStatus) keyPressMainWin(_ *gtk.Window, event *gdk.Event) {
 	log.Debug("KeyPress function called")
 	eventKey := gdk.EventKeyNewFromEvent(event)
 	// If pressed key is "Delete" key, remove selected files from the list
@@ -191,7 +303,7 @@ func (ui *UIStatus) removeSelected() (err error) {
 		value, err := listStore.GetValue(iter, fileFullPath)
 		if err != nil {
 			log.Debug(err)
-			log.Error("Error while getting value")
+			log.Error("Error while getting full path from iterator")
 			return
 		}
 		fullPath, err := value.GetString()
@@ -201,35 +313,91 @@ func (ui *UIStatus) removeSelected() (err error) {
 			return
 		}
 		log.Debug("Full path: ", fullPath)
-
+		value, err = listStore.GetValue(iter, fileSizeInBytes)
+		if err != nil {
+			log.Debug(err)
+			log.Error("Error while getting value from GetValue")
+			return
+		}
+		goValue, err := value.GoValue()
+		if err != nil {
+			log.Debug(err)
+			log.Error("Error while getting value from GoValue")
+			return
+		}
+		size, ok := goValue.(int64)
+		if !ok {
+			log.Debug(AssertFailed)
+			log.Error("Returned value is not int64")
+			return
+		}
 		// Delete from the map as well
 		delete(ui.fileMap, fullPath)
 		_ = listStore.Remove(iter)
+		ui.totalFileSize -= size
+		ui.totalFileCount -= 1
 	})
+	ui.updateInfoBox()
 	return nil
 }
 
-func addToListStore(listStore *gtk.ListStore, treeView *gtk.TreeView, dataToAdd []string, order []int, f preprocessing) {
-	for i, data := range dataToAdd {
-		row, err := f(data)
-		if err != nil {
-			log.Debug(err)
-			log.Error("Error while adding element. continuing")
+func (ui *UIStatus) updateInfoBox() {
+	fileCountLabel, err := ui.getLabelWithId("infoFileCount")
+	if err != nil {
+		return
+	}
+	fileSizeLabel, err := ui.getLabelWithId("infoFileSize")
+	if err != nil {
+		return
+	}
+	fileCountLabel.SetLabel(strconv.Itoa(ui.totalFileCount))
+	fileSizeLabel.SetLabel(sizeAddUnit(ui.totalFileSize))
+}
+
+func (ui *UIStatus) addFilesToListStore(fileNames []string) {
+	fileList, err := ui.getListStoreWithId("fileList")
+	if err != nil {
+		return
+	}
+	treeView, err := ui.getTreeViewWithId("fileListView")
+	if err != nil {
+		return
+	}
+
+	for _, fileName := range fileNames {
+		// Check if elem already exist
+		if _, exist := ui.fileMap[fileName]; exist {
+			log.Debug("Element already added; Skipping...")
 			continue
 		}
-		// Row is empty if the elem is already added
-		if row != nil {
-			iter := listStore.Append()
-			// Show file full path as a tooltip
-			treeView.SetTooltipColumn(fileFullPath)
-			if err = listStore.Set(iter, order, row); err != nil {
-				log.Debug("Error while adding ", dataToAdd[i])
-				continue
-			}
-		} else {
-			log.Debug("Element already added; Skipping...")
+		// Add to set
+		ui.fileMap[fileName] = struct{}{}
+
+		// Get file name without path
+		_, fName := filepath.Split(fileName)
+
+		// Get file size
+		s, err := os.Stat(fileName)
+		if err != nil {
+			log.Debug(err)
+			log.Error("Error while getting stats; Skipping...")
+			continue
 		}
+
+		size := s.Size()
+		row := []interface{}{fName, sizeAddUnit(size), "Pending", fileName, size}
+
+		iter := fileList.Append()
+		// Show file full path as a tooltip
+		treeView.SetTooltipColumn(fileFullPath)
+		if err = fileList.Set(iter, ui.fileListOrder, row); err != nil {
+			log.Debug("Error while adding ", fileName)
+			continue
+		}
+		ui.totalFileSize += size
+		ui.totalFileCount += 1
 	}
+	ui.updateInfoBox()
 }
 
 func (ui *UIStatus) getFileInfo(fileName string) ([]interface{}, error) {
@@ -257,7 +425,7 @@ func sizeAddUnit(size int64) (sizeStr string) {
 	} else if size < 1e+9 {
 		sizeStr = strconv.Itoa(int(size/1e+6)) + " MB"
 	} else {
-		sizeStr = strconv.Itoa(int(size/1e+9)) + " MB"
+		sizeStr = strconv.Itoa(int(size/1e+9)) + " GB"
 	}
 	return sizeStr
 }
@@ -266,6 +434,54 @@ func (ui *UIStatus) switchPage() {
 	log.Debug("Switch page clicked")
 	ui.isFileTab = !ui.isFileTab
 	log.Debug(ui.isFileTab)
+}
+
+func (ui *UIStatus) getPopoverWithId(popoverId string) (popover *gtk.Popover, err error) {
+	object, err := ui.builder.GetObject(popoverId)
+	if err != nil {
+		log.Debug(err)
+		log.Errorf("Error while getting popover with popover id: %s", popoverId)
+		return nil, err
+	}
+	popover, ok := object.(*gtk.Popover)
+	if ok {
+		return popover, nil
+	}
+	log.Debug(AssertFailed)
+	log.Error("object is not a popover")
+	return nil, AssertFailed
+}
+
+func (ui *UIStatus) getListBoxRowWithId(listBoxRowId string) (listBoxRow *gtk.ListBoxRow, err error) {
+	object, err := ui.builder.GetObject(listBoxRowId)
+	if err != nil {
+		log.Debug(err)
+		log.Errorf("Error while getting listBoxRow with listBoxRow id: %s", listBoxRowId)
+		return nil, err
+	}
+	listBoxRow, ok := object.(*gtk.ListBoxRow)
+	if ok {
+		return listBoxRow, nil
+	}
+	log.Debug(AssertFailed)
+	log.Error("object is not a listBoxRow")
+	return nil, AssertFailed
+}
+
+func (ui *UIStatus) getLabelWithId(labelId string) (label *gtk.Label, err error) {
+	object, err := ui.builder.GetObject(labelId)
+	if err != nil {
+		log.Debug(err)
+		log.Errorf("Error while getting label with label id: %s", labelId)
+		return nil, err
+	}
+	label, ok := object.(*gtk.Label)
+	if ok {
+		return label, nil
+	}
+	log.Debug(AssertFailed)
+	log.Error("object is not a label")
+	return nil, AssertFailed
 }
 
 func (ui *UIStatus) getButtonWithId(buttonId string) (button *gtk.Button, err error) {
