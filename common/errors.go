75b5b46beb94d3234d75f13ac20c5452c4e4e578
Jaeha Choi <jaeha220@gmail.com>
2021-08-07 16:47:03 -0700

    Add more errors and change orders

--- common/errors.go
+++ common/errors.go
@@ -23,10 +23,15 @@ func (e *Error) GetCode() (code uint8) { return e.ErrCode }
 var ErrorCodes = [255]*Error{
 	nil, // Error code 0 means no error
 	UnknownCodeError,
-	UnexpectedCommandError,
-	ExistingConnError,
-	ReceiverNotFound,
+	UnknownCommandError,
+	GeneralServerError,
 	TaskNotCompleteError,
+	PubKeyMismatchError,
+	ClientNotFoundError,
+	ReceiverNotFound,
+	ReceiverNotAvailable,
+	NoAvailableAddCodeError,
+	ExistingConnError,
 }
 
 var UnknownCodeError = &Error{
@@ -34,22 +39,47 @@ var UnknownCodeError = &Error{
 	ErrCode: 1,
 }
 
-var UnexpectedCommandError = &Error{
-	Err:     errors.New("unexpected command returned"),
+var UnknownCommandError = &Error{
+	Err:     errors.New("unknown command returned"),
 	ErrCode: 2,
 }
 
-var ExistingConnError = &Error{
-	Err:     errors.New("existing connection present in client struct"),
+var GeneralServerError = &Error{
+	Err:     errors.New("general server error"),
 	ErrCode: 3,
-}
-
-var ReceiverNotFound = &Error{
-	Err:     errors.New("receiver was not found"),
-	ErrCode: 4,
 }
 
 var TaskNotCompleteError = &Error{
 	Err:     errors.New("task not complete"),
+	ErrCode: 4,
+}
+
+var PubKeyMismatchError = &Error{
+	Err:     errors.New("public key mismatch"),
 	ErrCode: 5,
+}
+
+var ClientNotFoundError = &Error{
+	Err:     errors.New("client not found error"),
+	ErrCode: 6,
+}
+
+var ReceiverNotFound = &Error{
+	Err:     errors.New("receiver was not found"),
+	ErrCode: 7,
+}
+
+var ReceiverNotAvailable = &Error{
+	Err:     errors.New("receiver is not available"),
+	ErrCode: 8,
+}
+
+var NoAvailableAddCodeError = &Error{
+	Err:     errors.New("no available add code error"),
+	ErrCode: 9,
+}
+
+var ExistingConnError = &Error{
+	Err:     errors.New("existing connection present in client struct"),
+	ErrCode: 10,
 }
